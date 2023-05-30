32dab4c891d2b3e6bb3b0ace4e8a05e6401f4ec4
Rafael Sanzio <rafaelsanzio27@gmail.com>
2023-05-30 19:18:30 -0300

    feat: adding pkg scheduler to send message to websocket
    adding pkg scheduler to send message to websocket

---
+++ pkg/scheduler/scheduler.go
@@ -0,0 +1,66 @@
+package scheduler
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"log"
+	"math/rand"
+	"time"
+
+	"github.com/gorilla/websocket"
+
+	"github.com/rafaelsanzio/go-stock-exchange-data/data"
+	"github.com/rafaelsanzio/go-stock-exchange-data/pkg/applog"
+	"github.com/rafaelsanzio/go-stock-exchange-data/pkg/config"
+	"github.com/rafaelsanzio/go-stock-exchange-data/pkg/config/key"
+	"github.com/rafaelsanzio/go-stock-exchange-data/pkg/errs"
+)
+
+func Run(ctx context.Context) {
+	events := data.ReadJSONData()
+
+	wsURL, err_ := config.Value(key.WSURL)
+	if err_ != nil {
+		_ = errs.ErrGettingEnvWebSocketURL.Throwf(applog.Log, errs.ErrFmt, err_)
+	}
+
+	wsPort, err_ := config.Value(key.WSPort)
+	if err_ != nil {
+		_ = errs.ErrGettingEnvWebSocketPort.Throwf(applog.Log, errs.ErrFmt, err_)
+	}
+
+	topic, err_ := config.Value(key.Topic)
+	if err_ != nil {
+		_ = errs.ErrGettingEnvTopic.Throwf(applog.Log, errs.ErrFmt, err_)
+	}
+
+	wsListenerURL := fmt.Sprintf("%s:%s/ws?topic=%s", wsURL, wsPort, topic)
+
+	conn, _, err := websocket.DefaultDialer.Dial(wsListenerURL, nil)
+	if err != nil {
+		_ = errs.ErrWebSocketConnection.Throwf(applog.Log, errs.ErrFmt, err)
+	}
+	defer conn.Close()
+
+	ticker := time.NewTicker(10 * time.Second)
+	defer ticker.Stop()
+
+	for range ticker.C {
+		event := events[rand.Intn(len(events))]
+
+		eventJSON, err := json.Marshal(event)
+		if err != nil {
+			_ = errs.ErrMarshalingJson.Throwf(applog.Log, errs.ErrFmt, err)
+			continue
+		}
+
+		err = conn.WriteMessage(websocket.TextMessage, eventJSON)
+		if err != nil {
+			_ = errs.ErrWebSocketSendingMsg.Throwf(applog.Log, errs.ErrFmt, err)
+			continue
+		}
+
+		log.Printf("Sent message to websocket: %s\n", string(eventJSON))
+	}
+}
